44453fd3ad22224789b3d5e100545a3fc1a5a504
김솔비 <ssb1179@naver.com>
2022-10-16 23:18:19 +0900

    두번째 화면 만들고 커밋

--- app/src/main/java/silvertown/endyearpartygame/MainActivity.java
+++ app/src/main/java/silvertown/endyearpartygame/MainActivity.java
@@ -2,7 +2,11 @@ package silvertown.endyearpartygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //네비게이션 바 없애기 코드
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+        if (isImmersiveModeEnabled) {
+            Log.i("Is on?", "Turning immersive mode mode off. ");
+        } else {
+            Log.i("Is on?", "Turning immersive mode mode on.");
+        }
+        newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+//        newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+
+
+        //뷰에 있는 버튼 설정
+        Button gameStart = (Button) findViewById(R.id.btnGameStart);
+        gameStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GameChoiceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }

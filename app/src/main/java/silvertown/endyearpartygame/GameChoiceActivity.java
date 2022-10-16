7832d070e83337572c62db715407bf9e38677d02
김솔비 <ssb1179@naver.com>
2022-10-17 01:46:39 +0900

    두번째 화면 버튼 배치하고 설명부분 틀만 만듬

--- app/src/main/java/silvertown/endyearpartygame/GameChoiceActivity.java
+++ app/src/main/java/silvertown/endyearpartygame/GameChoiceActivity.java
@@ -1,6 +1,9 @@
 package silvertown.endyearpartygame;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,21 @@ public class GameChoiceActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_choice);
+
+        //네비게이션 바 없애기 코드
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+        if (isImmersiveModeEnabled) {
+            Log.i("Is on?", "Turning immersive mode mode off. ");
+        } else {
+            Log.i("Is on?", "Turning immersive mode mode on.");
+        }
+        newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+//        newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+
+
     }
 }

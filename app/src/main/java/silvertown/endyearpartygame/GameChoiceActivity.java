6777d11de287e46b88f2f68869d8671161ea46ba
김솔비 <ssb1179@naver.com>
2022-11-06 19:57:15 +0900

    버튼클릭시 일부전환 그냥 visible로 해결했고 버튼 클릭시에 버튼색 전환에대해서 메소드 고민해보기

--- app/src/main/java/silvertown/endyearpartygame/GameChoiceActivity.java
+++ app/src/main/java/silvertown/endyearpartygame/GameChoiceActivity.java
@@ -1,8 +1,11 @@
 package silvertown.endyearpartygame;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +32,46 @@ public class GameChoiceActivity extends AppCompatActivity {
         newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
 
+        // 선택된 버튼 배경색 : @drawable/middle_color_frame
+        // 선택된 버튼 글자색 : @color/lightColor
+        // 선택안된 버튼 배경색 : @drawable/menu_button
+        // 선택안된 버튼 글자색 : Color.BLACK)
 
+
+
+
+        //이어말하기 화면
+        FrameLayout frame_relay = findViewById(R.id.frame_relay);
+
+        //이어말하기 클릭시 화면 일부 전환
+        Button btn_relay = findViewById(R.id.btn_relay);
+        btn_relay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                frame_relay.setVisibility(View.VISIBLE);
+                btn_relay.setBackgroundResource(R.drawable.menu_button);
+                btn_relay.setTextColor(Color.BLACK);
+            }
+        });
+//@drawable/menu_button
+
+
+        //초성게임 클릭시 화면 일부 전환
+        Button btn_chosung = findViewById(R.id.btn_chosung);
+        btn_chosung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                frame_relay.setVisibility(View.INVISIBLE);
+                btn_chosung.setBackgroundResource(R.drawable.middle_color_frame);
+                btn_chosung.setTextColor(getResources().getColorStateList(R.color.lightColor));
+            }
+        });
+
+
+    }
+
+    public void backColorChange(String btnName) {
+
+        // 파라미터로 들어오는 버튼이름으로 색 변환시키기
     }
 }
